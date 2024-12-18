a4a3986d2715fb8adfbd02f8b3cc07f15ab0ed1e
Jorge Vilanova <vilanova.alejandro50@gmail.com>
2024-12-18 15:09:14 -0800

    not sure why its saying I am using useState

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import React from 'react';
 import { FaHouseUser } from 'react-icons/fa6';
 import { FaCalendarAlt } from 'react-icons/fa';
 import { IoSettingsSharp } from 'react-icons/io5';
